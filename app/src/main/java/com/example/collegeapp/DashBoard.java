f51d38f4e23b38b2c566e4404dfbb30343811ed7
vanimnaoj <ansumariam99@gmail.com>
2022-02-15 15:16:05 +0530

    WebView and SharedPreference Completed

--- app/src/main/java/com/example/collegeapp/DashBoard.java
+++ app/src/main/java/com/example/collegeapp/DashBoard.java
@@ -4,11 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 
 public class DashBoard extends AppCompatActivity {
     AppCompatButton b1,b2,b3,b4,b5,b6;
+    SharedPreferences myPrefer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +22,7 @@ public class DashBoard extends AppCompatActivity {
         b4=(AppCompatButton) findViewById(R.id.sf);
         b5=(AppCompatButton) findViewById(R.id.vb);
         b6=(AppCompatButton) findViewById(R.id.logout);
+        myPrefer=getSharedPreferences("login",MODE_PRIVATE);
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -58,6 +61,9 @@ public class DashBoard extends AppCompatActivity {
         b6.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                SharedPreferences.Editor edit=myPrefer.edit();
+                edit.clear();
+                edit.commit();
                 Intent i=new Intent(getApplicationContext(),MainActivity.class);
                 startActivity(i);
             }
--- app/src/main/java/com/example/collegeapp/MainActivity.java
+++ app/src/main/java/com/example/collegeapp/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -12,25 +13,36 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
     EditText ed1,ed2;
     AppCompatButton b1,b2;
-    String getemail,getpass;
+    String getEmail,getPass,preferValue;
+    SharedPreferences myprefer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        myprefer=getSharedPreferences("login",MODE_PRIVATE);
         ed1=(EditText) findViewById(R.id.email);
         ed2=(EditText) findViewById(R.id.pass);
         b1=(AppCompatButton) findViewById(R.id.lb);
         b2=(AppCompatButton) findViewById(R.id.regb);
+        preferValue=myprefer.getString("email",null);//read
+        if(preferValue!=null)
+        {
+            Intent i=new Intent(getApplicationContext(),DashBoard.class);
+            startActivity(i);
+        }
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                getemail=ed1.getText().toString();
-                getpass=ed2.getText().toString();
-                Toast.makeText(getApplicationContext(),getemail, Toast.LENGTH_SHORT).show();
-                Toast.makeText(getApplicationContext(),getpass,Toast.LENGTH_SHORT).show();
-                if (getemail.equals("admin@gmail.com")&&(getpass.equals("12345")))
+                getEmail=ed1.getText().toString();
+                getPass=ed2.getText().toString();
+                Toast.makeText(getApplicationContext(),getEmail, Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(),getPass,Toast.LENGTH_SHORT).show();
+                if (getEmail.equals("admin@gmail.com")&&(getPass.equals("12345")))
                 {
+                    SharedPreferences.Editor myEdit=myprefer.edit();
+                    myEdit.putString("email",getEmail);
+                    myEdit.commit();
                     Intent i=new Intent(getApplicationContext(),DashBoard.class);
                     startActivity(i);
                 }
